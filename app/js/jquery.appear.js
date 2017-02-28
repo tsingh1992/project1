e204fefbe9ac1c08422920829e4136c013ea8eb6
tsingh1992 <tks5535@hotmail.com>
2017-02-28 18:13:16 -0500

    First commit

--- app/js/jquery.appear.js
+++ app/js/jquery.appear.js
@@ -1,13 +1,4 @@
-/*
- * jQuery appear plugin
- *
- * Copyright (c) 2012 Andrey Sidorov
- * licensed under MIT license.
- *
- * https://github.com/morr/jquery.appear/
- *
- * Version: 0.3.3
- */
+
 (function($) {
   var selectors = [];
 
